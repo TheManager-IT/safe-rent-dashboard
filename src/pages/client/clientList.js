c55a6c368f05b46476914bb692def657bc3415ee
Jihene Najjar <jihenenajjar123@gmail.com>
2024-04-21 03:56:01 +0100

    mise en forme listeClients

--- src/pages/client/clientList.js
+++ src/pages/client/clientList.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import Scrollbar from '../../components/scrollbar';
+
 import {
   Button,
   Container,
@@ -10,14 +13,19 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Stack,
+  Card,
   OutlinedInput,
   InputAdornment,
-  IconButton,Typography,
+  IconButton,Typography,TablePagination,
 } from '@mui/material';
 
 const Clients = () => {
   const [clients, setClients] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(0);
+const [rowsPerPage, setRowsPerPage] = useState(5);
+
 
   useEffect(() => {
     fetch('http://localhost:3000/v1/api/client')
@@ -51,7 +59,7 @@ const Clients = () => {
     }
   };
 
-
+/*
   const filteredData = clients.filter(client =>
     client.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
     client.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -59,16 +67,30 @@ const Clients = () => {
     client.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
     client.nationalID.toLowerCase().includes(searchTerm.toLowerCase()) ||
     client.address.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  );*/
+  const filteredData = clients.filter(client =>
+    client.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.phoneNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.nationalID.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    client.address.toLowerCase().includes(searchTerm.toLowerCase())
+  ).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  
 
   return (
     <Container>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+
      <Typography variant="h4" sx={{ mb: 2 }}>
         Clients
       </Typography>
-
-      
-   
+      <Link to="/addClient">
+          <Button variant="contained" color="primary" onClick={handleAddClient}>
+            Ajouter
+          </Button>
+        </Link> 
+        </Stack>
 
     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
       <OutlinedInput
@@ -84,57 +106,74 @@ const Clients = () => {
         }
       />
         
-      <div>  
-       <Link to="/addClient">
-          <Button variant="contained" color="primary" onClick={handleAddClient}>
-            Ajouter
-          </Button>
-        </Link> 
-      </div>  
-</div>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Nom</TableCell>
-              <TableCell>Prénom</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Numéro de Téléphone</TableCell>
-              <TableCell>CIN</TableCell>
-              <TableCell>Adresse</TableCell>
-              <TableCell>Modifier</TableCell>
-              <TableCell>Supprimer</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredData.map((client) => (
-              <TableRow key={client._id}>
-                <TableCell>{client._id}</TableCell>
-                <TableCell>{client.name}</TableCell>
-                <TableCell>{client.firstName}</TableCell>
-                <TableCell>{client.email}</TableCell>
-                <TableCell>{client.phoneNumber}</TableCell>
-                <TableCell>{client.nationalID}</TableCell>
-                <TableCell>{client.address}</TableCell>
-                <TableCell>
-                  <Link to={`/editClient/${client._id}`}>
-                    <Button variant="contained" color="primary" onClick={() => handleEdit(client._id)}>
-                      Modifier
-                    </Button>
-                  </Link>
-                </TableCell>
-                <TableCell>
-                  <Button variant="contained" color="secondary" onClick={() => handleDelete(client._id)}>
-                    Supprimer
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
    
+</div>
+
+<Card>
+        
+<Scrollbar>
+  
+  <TableContainer sx={{ overflow: 'unset' }}>
+  <Table sx={{ minWidth: 800 }}>
+    <TableHead>
+      <TableRow>
+        
+        <TableCell>Nom</TableCell>
+        <TableCell>Prénom</TableCell>
+        <TableCell>Email</TableCell>
+        <TableCell>Numéro de Téléphone</TableCell>
+        <TableCell>CIN</TableCell>
+        <TableCell>Adresse</TableCell>
+        <TableCell>Modifier</TableCell>
+        <TableCell>Supprimer</TableCell>
+      </TableRow>
+    </TableHead>
+    
+    <TableBody>
+      {filteredData.map((client) => (
+        <TableRow key={client._id}>
+          
+          <TableCell>{client.name}</TableCell>
+          <TableCell>{client.firstName}</TableCell>
+          <TableCell>{client.email}</TableCell>
+          <TableCell>{client.phoneNumber}</TableCell>
+          <TableCell>{client.nationalID}</TableCell>
+          <TableCell>{client.address}</TableCell>
+          <TableCell>
+            <Link to={`/editClient/${client._id}`}>
+              <Button variant="contained" color="primary" onClick={() => handleEdit(client._id)}>
+                Modifier
+              </Button>
+            </Link>
+          </TableCell>
+          <TableCell>
+            <Button variant="contained" color="secondary" onClick={() => handleDelete(client._id)}>
+              Supprimer
+              </Button>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+
+
+  <TablePagination
+    component="div"
+    count={clients.length} // total number of rows
+    rowsPerPage={rowsPerPage}
+    page={page}
+    onPageChange={(event, newPage) => setPage(newPage)}
+    onRowsPerPageChange={(event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0); // Reset page to 0 when rowsPerPage changes
+    }}
+    rowsPerPageOptions={[5, 10, 25]}
+  />
+</TableContainer>
+
+</Scrollbar>
+ 
+ </Card>
 </Container>
   );
 };
