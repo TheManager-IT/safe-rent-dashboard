422f1b137123725b1455ed8d18596cb26f2334ae
marwa <marwaneji54@gmail.com>
2024-05-04 19:37:41 +0200

    rapport voiture

--- src/pages/cars/CarDetail.js
+++ src/pages/cars/CarDetail.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Button, Checkbox, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import './CarDetail.css'; 
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import TroubleshootSharpIcon from '@mui/icons-material/TroubleshootSharp';
+import { PDFDownloadLink, Document, Page, Text, Image } from '@react-pdf/renderer'; 
+
 const CarDetail = () => {
   const [car, setCar] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [fields, setFields] = useState({
+    registrationPlate: true,
+    brand: true,
+    model: true,
+    locationPrice: true,
+    mileage: true,
+    status: true
+  });
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -24,9 +36,42 @@ const CarDetail = () => {
     // Logique de suppression ici
   };
 
+  const generatePDF = () => {
+    setOpen(true);
+  };
+
   if (!car) {
     return <div>Loading...</div>;
   }
+
+  const MyDocument = () => (
+    <Document>
+      <Page>
+        {fields.registrationPlate && <Text>Immatriculation: {car.registrationPlate}</Text>}
+        {fields.brand && <Text>Marque: {car.brand}</Text>}
+        {fields.model && <Text>Modèle: {car.model}</Text>}
+        {fields.locationPrice && <Text>Prix de la location par jour: {car.locationPrice}</Text>}
+        {fields.mileage && <Text>Kilométrage: {car.traveled.at(-1).mileage}</Text>}
+        {fields.status && <Text>Statut: {car.status}</Text>}
+        <Image src={`http://localhost:3000/uploads/${car.images}`} />
+      </Page>
+    </Document>
+  );
+
+  const handleFieldChange = (field) => {
+    setFields(prevFields => ({
+      ...prevFields,
+      [field]: !prevFields[field]
+    }));
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleEdit = (id) => {
+    console.log('Edit car with id:', id);
+  };
+
 
   return (
     <Container>
@@ -34,25 +79,47 @@ const CarDetail = () => {
        {car.model}
       </Typography>
       <div className="car-details-container">
+        <img src={`http://localhost:3000/uploads/${car.images}`} alt="Car Image" />
         <Typography> <b> Immatriculation:</b> {car.registrationPlate}</Typography>
         <Typography> <b> Marque:</b> {car.brand}</Typography>
         <Typography> <b> Modèle:</b> {car.model}</Typography>
         <Typography> <b>Prix de la location par jour: </b>  {car.locationPrice}</Typography>
-        <img src={`http://localhost:3000/uploads/${car.images}`} alt="Car Image" />
-        <Typography> <b> Kilométrage:</b>  {car.traveled.mileage}</Typography>
-        <Typography> <b> Statut:</b>  {car.status}</Typography>
-
+        <Typography><b> Kilométrage: </b> {car.traveled.at(-1).mileage}</Typography>
        
         <div className="button-group">
-          <Button variant="contained" component={Link} to={`/editCar/${car.id}`} startIcon={<EditIcon />} sx={{ mr: 1 }}>
-            Modifier
-          </Button>
+          <Link to={`/editCar/${car._id}`}>
+            <Button onClick={() => handleEdit(car._id)} variant="contained" color="primary"  startIcon={<EditIcon />} sx={{ mr: 1 }}>
+              Modifier
+            </Button>
+          </Link>
           <Button variant="contained" color="error" onClick={handleDelete} startIcon={<DeleteIcon />}>
             Supprimer
           </Button>
           <Button variant="contained" color="info" startIcon={<TroubleshootSharpIcon  />}>
             Diagnostic
           </Button>
+          <Button variant="contained" color="info" onClick={generatePDF}>
+            Télécharger PDF
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Choisir les champs pour le PDF</DialogTitle>
+            <DialogContent>
+              {Object.keys(fields).map((field, index) => (
+                <div key={index}>
+                  <Checkbox checked={fields[field]} onChange={() => handleFieldChange(field)} />
+                  <Typography>{field}</Typography>
+                </div>
+              ))}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Annuler</Button>
+              <PDFDownloadLink document={<MyDocument />} fileName="car-details.pdf">
+                {({ blob, url, loading, error }) =>
+                  loading ? 'Chargement du PDF...' : 'Télécharger le PDF'
+                }
+              </PDFDownloadLink>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     </Container>
